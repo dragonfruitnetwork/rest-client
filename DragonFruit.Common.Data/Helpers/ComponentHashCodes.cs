29734311b35c5df012fc419194ce9937fd2aa90b
Albie <admin@dragonfruit.network>
2020-03-21 19:18:00 +0000

    make hash code extension public

--- DragonFruit.Common.Data/Helpers/ComponentHashCodes.cs
+++ DragonFruit.Common.Data/Helpers/ComponentHashCodes.cs
@@ -4,8 +4,8 @@ using System.Text;
 
 namespace DragonFruit.Common.Data.Helpers
 {
-    internal static class ComponentHashCodes
+    public static class ComponentHashCodes
     {
-        internal static string ItemHashCode(this object data) => data == null ? "!" : data.GetHashCode().ToString();
+        public static string ItemHashCode(this object data) => data == null ? "!" : data.GetHashCode().ToString();
     }
 }
