7a634aa4c0d6a4d04d7d4239eb87ab310c0bae0b
Albie <admin@dragonfruit.network>
2020-07-12 14:51:59 +0100

    Update Nuget.ps1

--- Nuget.ps1
+++ Nuget.ps1
@@ -1,17 +1,22 @@
-param ([string]$Suffix = "")
+param ([string] $ApiKey, [string]$Suffix = "")
 
 #versioning info
-$DATE = Get-Date -UFormat "%Y.%m%d"
-$VERSION = "$($DATE).$($env:GITHUB_RUN_NUMBER)$($Suffix)"
-
-#directories
+$VERSION = "$(Get-Date -UFormat "%Y.%m%d").$($env:GITHUB_RUN_NUMBER)$($Suffix)"
 $WORKINGDIR = Get-Location
 
+#Build files
+
+Write-Output "Building Orbit Version $VERSION"
+dotnet restore
+dotnet build -c Release /p:PackageVersion=$VERSION
+
 #pack into nuget files with the suffix if we have one
-Write-Output "Publishing DragonFruit.Common.Data (Version $VERSION)"
-dotnet pack ".\DragonFruit.Common.Data\DragonFruit.Common.Data.csproj" -o $WORKINGDIR -c Release -p:PackageVersion=$VERSION
+
+Write-Output "Publishing Orbit Version $VERSION"
+dotnet pack ".\DragonFruit.Orbit.API\DragonFruit.Orbit.API.csproj" -o $WORKINGDIR -c Release -p:PackageVersion=$VERSION
 
 #recursively push all nuget files created
+
 Get-ChildItem -Path $WORKINGDIR -Filter *.nupkg -Recurse -File -Name | ForEach-Object {
-    dotnet nuget push $_ --api-key $env:nuget_key --source https://api.nuget.org/v3/index.json --force-english-output
+    dotnet nuget push $_ --api-key $ApiKey --source https://api.nuget.org/v3/index.json --force-english-output
 }
