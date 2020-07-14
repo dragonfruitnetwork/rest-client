0ff719ffd2505ecc9165c7d502791a214fba1814
Albie <albion@dragonfruit.network>
2020-07-14 12:24:33 +0100

    slightly alter test

--- DragonFruit.Common.Data.Tests/Handlers/AuthPreservingHandler/HeaderPreservingHeaderTests.cs
+++ DragonFruit.Common.Data.Tests/Handlers/AuthPreservingHandler/HeaderPreservingHeaderTests.cs
@@ -1,7 +1,6 @@
 ﻿// DragonFruit.Common Copyright 2020 DragonFruit Network
 // Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
 
-using System.Collections.Generic;
 using DragonFruit.Common.Data.Tests.Handlers.AuthPreservingHandler.Objects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -14,20 +13,24 @@ namespace DragonFruit.Common.Data.Tests.Handlers.AuthPreservingHandler
         public void TestHeaderPreservation()
         {
             var redirectClient = new HeaderPreservingHandlerClient();
+            ApiRequest tokenRequest;
 
             try
             {
-                //get token
-                var auth = redirectClient.Perform<BasicOrbitAuthResponse>(new AuthRequest());
-                redirectClient.Authorization = $"{auth.Type} {auth.AccessToken}";
-
-                //user lookups by username = 301. without our HeaderPreservingHandler we'd get a 401
-                redirectClient.Perform(new OrbitTestUserRequest());
+                tokenRequest = new AuthRequest();
             }
-            catch (KeyNotFoundException)
+            catch
             {
                 Assert.Inconclusive("not able to test due to inaccessible secrets.");
+                return;
             }
+
+            //get auth token
+            var auth = redirectClient.Perform<BasicOrbitAuthResponse>(tokenRequest);
+            redirectClient.Authorization = $"{auth.Type} {auth.AccessToken}";
+
+            //user lookups by username = 301. without our HeaderPreservingHandler we'd get a 401
+            redirectClient.Perform(new OrbitTestUserRequest());
         }
     }
 }
