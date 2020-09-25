51408918e8282b176ab4366efe3e4d6395b75eb7
Albie <albion@dragonfruit.network>
2020-09-25 19:27:23 +0100

    migrate parts of the request file to util files and add a basicapirequest builder with a sort of fluent api

--- DragonFruit.Common.Data/ApiRequest.cs
+++ DragonFruit.Common.Data/ApiRequest.cs
@@ -4,13 +4,12 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Reflection;
 using DragonFruit.Common.Data.Exceptions;
 using DragonFruit.Common.Data.Parameters;
 using DragonFruit.Common.Data.Serializers;
+using DragonFruit.Common.Data.Utils;
 using Newtonsoft.Json;
 
 namespace DragonFruit.Common.Data
@@ -19,7 +18,7 @@ namespace DragonFruit.Common.Data
     public abstract class ApiRequest
     {
         /// <summary>
-        /// The Path to the web resource
+        /// The path (including host, protocol and non-standard port) to the web resource
         /// </summary>
         public abstract string Path { get; }
 
@@ -52,11 +51,15 @@ namespace DragonFruit.Common.Data
         /// <summary>
         /// The fully compiled url
         /// </summary>
-        public string FullUrl => Path + QueryString;
+        public string FullUrl => UrlCompiler;
+
+        /// <summary>
+        /// Getter for fully compiled url (internally visible)
+        /// </summary>
+        internal virtual string UrlCompiler => Path + QueryString;
 
         /// <summary>
         /// Overridable property for configuring a custom body for this request
-        ///
         /// <para>
         /// Only used when the <see cref="BodyType"/> is equal to <see cref="BodyType.Custom"/>
         /// </para>
@@ -64,57 +67,23 @@ namespace DragonFruit.Common.Data
         protected virtual HttpContent BodyContent { get; }
 
         /// <summary>
-        /// <see cref="CultureInfo"/> used for ToString() conversions when collecting attributed members
+        /// Overridable culture for serialising requests.
+        /// Defaults to <see cref="CultureUtils.DefaultCulture"/>
         /// </summary>
-        protected virtual CultureInfo Culture => CultureInfo.InvariantCulture;
+        protected virtual CultureInfo RequestCulture => CultureUtils.DefaultCulture;
 
-        internal string QueryString
-        {
-            get
-            {
-                var queries = GetParameter<QueryParameter>();
-                return !queries.Any()
-                    ? null
-                    : $"?{string.Join("&", queries.Select(kvp => $"{kvp.Key}={kvp.Value}"))}";
-            }
-        }
+        /// <summary>
+        /// Query string generated from all filled <see cref="QueryParameter"/>-attributed properties
+        /// </summary>
+        internal string QueryString => QueryUtils.QueryStringFrom(ParameterUtils.GetParameter<QueryParameter>(this, RequestCulture));
 
-        internal IEnumerable<KeyValuePair<string, string>> GetParameter<T>() where T : IProperty
-        {
-            var type = typeof(T);
-
-            foreach (var property in GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
-            {
-                if (!(Attribute.GetCustomAttribute(property, type) is T parameter))
-                {
-                    continue;
-                }
-
-                var value = property.GetValue(this, null);
-                string convertedValue = value switch
-                {
-                    bool boolVar => boolVar.ToString().ToLower(Culture),
-                    null => null,
-
-                    _ => value.ToString()
-                };
-
-                if (convertedValue != null)
-                {
-                    yield return new KeyValuePair<string, string>(parameter.Name, convertedValue);
-                }
-            }
-        }
-
-        internal object GetSingleParameterObject<T>() where T : Attribute =>
-            GetType().GetProperties()
-                     .Single(x => Attribute.GetCustomAttribute(x, typeof(T)) is T)
-                     .GetValue(this, null);
-
+        /// <summary>
+        /// Create a <see cref="HttpResponseMessage"/> for this <see cref="ApiRequest"/>, which can then be modified manually or overriden by <see cref="ApiClient.SetupRequest"/>
+        /// </summary>
         public HttpRequestMessage Build(ApiClient client) => Build(client.Serializer);
 
         /// <summary>
-        /// Creates a <see cref="HttpResponseMessage"/> for this <see cref="ApiRequest"/>, which can then be modified manually or overriden by <see cref="ApiClient.SetupRequest"/>
+        /// Create a <see cref="HttpResponseMessage"/> for this <see cref="ApiRequest"/>, which can then be modified manually or overriden by <see cref="ApiClient.SetupRequest"/>
         /// </summary>
         /// <remarks>
         /// This validates the <see cref="Path"/> and <see cref="RequireAuth"/> properties, throwing a <see cref="ClientValidationException"/> if it's unsatisfied with the constraints
@@ -167,14 +136,17 @@ namespace DragonFruit.Common.Data
                     throw new NotImplementedException();
             }
 
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(serializer.ContentType));
-
             if (Headers.IsValueCreated)
             {
                 foreach (var header in Headers.Value)
                 {
                     request.Headers.Add(header.Key, header.Value);
                 }
+            }
+
+            if (!request.Headers.Contains("Accept"))
+            {
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(serializer.ContentType));
             }
 
             return request;
@@ -185,13 +157,13 @@ namespace DragonFruit.Common.Data
             switch (BodyType)
             {
                 case BodyType.Encoded:
-                    return new FormUrlEncodedContent(GetParameter<FormParameter>());
+                    return new FormUrlEncodedContent(ParameterUtils.GetParameter<FormParameter>(this, RequestCulture));
 
                 case BodyType.Serialized:
                     return serializer.Serialize(this);
 
                 case BodyType.SerializedProperty:
-                    var body = serializer.Serialize(GetSingleParameterObject<RequestBody>());
+                    var body = serializer.Serialize(ParameterUtils.GetSingleParameterObject<RequestBody>(this));
                     return body;
 
                 case BodyType.Custom:
