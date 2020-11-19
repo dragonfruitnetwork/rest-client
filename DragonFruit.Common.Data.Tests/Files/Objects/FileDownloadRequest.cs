0de41e613686aa24d67289237a59133c076ed3ab
Albie <albion@dragonfruit.network>
2020-11-19 18:16:09 +0000

    use system throw for success code check

--- DragonFruit.Common.Data.Tests/Files/Objects/FileDownloadRequest.cs
+++ DragonFruit.Common.Data.Tests/Files/Objects/FileDownloadRequest.cs
@@ -6,8 +6,16 @@ namespace DragonFruit.Common.Data.Tests.Files.Objects
     /// <summary>
     /// Simple file download request with a path and a destination
     /// </summary>
-    public class FileDownloadRequest : ApiFileRequest
+    internal sealed class FileDownloadRequest : ApiFileRequest
     {
+        public FileDownloadRequest()
+        {
+            if (System.IO.File.Exists(Destination))
+            {
+                System.IO.File.Delete(Destination);
+            }
+        }
+
         public override string Path => "https://github.com/ppy/osu/archive/2020.327.0.zip";
 
         public override string Destination => System.IO.Path.Combine(System.IO.Path.GetTempPath(), "file.zip");
