534a444ab3c60e3a209132de8046c3e6e5db5d15
Albie <albion@dragonfruit.network>
2021-01-11 10:39:13 +0000

    add file download progress reporting

--- DragonFruit.Common.Data.Tests/Files/FileDownloadTests.cs
+++ DragonFruit.Common.Data.Tests/Files/FileDownloadTests.cs
@@ -1,6 +1,7 @@
 ﻿// DragonFruit.Common Copyright 2020 DragonFruit Network
 // Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
 
+using System;
 using System.IO;
 using DragonFruit.Common.Data.Basic;
 using NUnit.Framework;
@@ -10,19 +11,29 @@ namespace DragonFruit.Common.Data.Tests.Files
     [TestFixture]
     public class FileDownloadTests : ApiTest
     {
-        [TestCase]
-        public void FileDownloadTest()
+        [TestCase("https://github.com/ppy/osu/archive/2020.1121.0.zip", 19018589)]
+        public void FileDownloadTest(string path, long expectedFileSize)
         {
-            var request = new BasicApiFileRequest("https://github.com/ppy/osu/archive/2020.1121.0.zip", Path.GetTempPath());
+            var request = new BasicApiFileRequest(path, Path.GetTempPath());
 
-            Assert.IsFalse(File.Exists(request.Destination));
+            if (File.Exists(request.Destination))
+            {
+                try
+                {
+                    File.Delete(request.Destination);
+                }
+                catch
+                {
+                    Assert.Inconclusive("Failed to remove file needed for test");
+                }
+            }
 
             try
             {
-                Client.Perform(request);
+                Client.Perform(request, (progress, total) => TestContext.Out.WriteLine($"Progress: {progress:n0}/{total:n0} ({Convert.ToSingle(progress) / Convert.ToSingle(total):F2}%)"));
 
                 Assert.IsTrue(File.Exists(request.Destination));
-                Assert.IsTrue(new FileInfo(request.Destination).Length > 5000);
+                Assert.GreaterOrEqual(new FileInfo(request.Destination).Length, expectedFileSize);
             }
             finally
             {
--- DragonFruit.Common.Data/ApiFileRequest.cs
+++ DragonFruit.Common.Data/ApiFileRequest.cs
@@ -7,10 +7,23 @@ namespace DragonFruit.Common.Data
 {
     public abstract class ApiFileRequest : ApiRequest
     {
-        protected override Methods Method => Methods.Get;
-
+        /// <summary>
+        /// The location, on the disk, to put the resultant file
+        /// </summary>
         public abstract string Destination { get; }
 
+        /// <summary>
+        /// The mode of file creation
+        /// </summary>
         public virtual FileMode FileCreationMode => FileMode.Create;
+
+        /// <summary>
+        /// Length, in bytes, of the buffer used for copying data from the network stream to the file.
+        /// Defaults to 32kb
+        /// </summary>
+        /// <remarks>
+        /// This should be less than 85KiB (87040 bytes) to avoid potential memory leaks
+        /// </remarks>
+        public virtual int BufferSize => 32 * 1024;
     }
 }
