56b327a56149e17f54c7502ad6dbda46909cdf75
Albie <admin@dragonfruit.network>
2021-08-21 21:47:06 +0100

    Update ApiJsonSerializer.cs

--- DragonFruit.Common.Data/Serializers/ApiJsonSerializer.cs
+++ DragonFruit.Common.Data/Serializers/ApiJsonSerializer.cs
@@ -64,7 +64,7 @@ namespace DragonFruit.Common.Data.Serializers
 
         public Encoding Encoding
         {
-            get => _encoding ?? new UTF8Encoding(false);
+            get => _encoding ?? Encoding.UTF8;
             set => _encoding = value;
         }
 
