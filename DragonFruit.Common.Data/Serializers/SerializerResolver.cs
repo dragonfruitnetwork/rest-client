56201ba725b011bf19b089d4a6a4902c64b4d0b3
Albie <albion@dragonfruit.network>
2021-12-04 17:42:42 +0000

    fix invalid xmldoc

--- DragonFruit.Common.Data/Serializers/SerializerResolver.cs
+++ DragonFruit.Common.Data/Serializers/SerializerResolver.cs
@@ -66,7 +66,7 @@ namespace DragonFruit.Common.Data.Serializers
         }
 
         /// <summary>
-        /// The default <see cref="ISerializer"/> in use.
+        /// The default <see cref="ApiSerializer"/> in use.
         /// </summary>
         public ApiSerializer Default { get; }
 
