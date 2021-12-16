04c8cdfe11c67b06f82581ebd0057e13e3dd45e6
Albie <albion@dragonfruit.network>
2021-12-16 18:31:57 +0000

    add system.json serializer

---
+++ serializers/DragonFruit.Data.Serializers.SystemJson/ApiSystemTextJsonSerializer.cs
@@ -0,0 +1,33 @@
+﻿// DragonFruit.Data Copyright DragonFruit Network
+// Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
+
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DragonFruit.Data.Serializers.SystemJson
+{
+    public class ApiSystemTextJsonSerializer : ApiSerializer, IAsyncSerializer
+    {
+        public override string ContentType => "application/json";
+
+        // System.Text.Json doesn't accept different encoding
+        public override Encoding Encoding => Encoding.UTF8;
+
+        public JsonSerializerOptions SerializerOptions { get; set; }
+
+        public override HttpContent Serialize<T>(T input)
+        {
+            var stream = GetStream(false);
+            JsonSerializer.Serialize(stream, input, SerializerOptions);
+
+            return GetHttpContent(stream);
+        }
+
+        public override T Deserialize<T>(Stream input) => JsonSerializer.Deserialize<T>(input, SerializerOptions);
+
+        public Task<T> DeserializeAsync<T>(Stream input) where T : class => JsonSerializer.DeserializeAsync<T>(input, SerializerOptions).AsTask();
+    }
+}
