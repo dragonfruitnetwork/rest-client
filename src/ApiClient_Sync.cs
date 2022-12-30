94bd4c040b66d65616fa9a90d74086f411d1e8dc
Albie <albion@dragonfruit.network>
2022-12-30 11:00:30 +0000

    mark sync methods as obsolete

--- src/ApiClient_Sync.cs
+++ src/ApiClient_Sync.cs
@@ -9,9 +9,12 @@ namespace DragonFruit.Data
 {
     public partial class ApiClient
     {
+        private const string SyncObsoletionMessage = "Synchronous client operations will be removed in a future update. Change to Async variations where possible or block with .Wait()/.Result";
+
         /// <summary>
         /// Perform a request to the specified <see cref="url"/> that returns a strongly-typed class.
         /// </summary>
+        [Obsolete(SyncObsoletionMessage)]
         public T Perform<T>(string url, CancellationToken token = default) where T : class
         {
             return PerformAsync<T>(url, token).Result;
@@ -20,6 +23,7 @@ namespace DragonFruit.Data
         /// <summary>
         /// Perform an <see cref="ApiRequest"/> with a specified return type.
         /// </summary>
+        [Obsolete(SyncObsoletionMessage)]
         public T Perform<T>(ApiRequest requestData, CancellationToken token = default) where T : class
         {
             return PerformAsync<T>(requestData, token).Result;
@@ -28,6 +32,7 @@ namespace DragonFruit.Data
         /// <summary>
         /// Perform a pre-fabricated <see cref="HttpRequestMessage"/> and deserialize the result to the specified type
         /// </summary>
+        [Obsolete(SyncObsoletionMessage)]
         public T Perform<T>(HttpRequestMessage request, CancellationToken token = default) where T : class
         {
             return PerformAsync<T>(request, token).Result;
@@ -36,6 +41,7 @@ namespace DragonFruit.Data
         /// <summary>
         /// Perform a request to the specified <see cref="url"/> that returns a <see cref="HttpResponseMessage"/>.
         /// </summary>
+        [Obsolete(SyncObsoletionMessage)]
         public HttpResponseMessage Perform(string url, CancellationToken token = default)
         {
             return PerformAsync(url, token).Result;
@@ -44,6 +50,7 @@ namespace DragonFruit.Data
         /// <summary>
         /// Perform a <see cref="ApiRequest"/> that returns the response message.
         /// </summary>
+        [Obsolete(SyncObsoletionMessage)]
         public HttpResponseMessage Perform(ApiRequest requestData, CancellationToken token = default)
         {
             return PerformAsync(requestData, token).Result;
@@ -52,6 +59,7 @@ namespace DragonFruit.Data
         /// <summary>
         /// Perform a pre-fabricated <see cref="HttpRequestMessage"/>
         /// </summary>
+        [Obsolete(SyncObsoletionMessage)]
         public HttpResponseMessage Perform(HttpRequestMessage request, CancellationToken token = default)
         {
             return PerformAsync(request, token).Result;
@@ -63,6 +71,7 @@ namespace DragonFruit.Data
         /// <remarks>
         /// Bypasses <see cref="ValidateAndProcess{T}"/>
         /// </remarks>
+        [Obsolete(SyncObsoletionMessage)]
         public void Perform(ApiFileRequest request, Action<long, long?> progressUpdated = null, CancellationToken token = default)
         {
             PerformAsync(request, progressUpdated, token).Wait(token);
