edf3607e59be8625a34d7d66c2894785c03bcbad
Albie <albion@dragonfruit.network>
2023-02-10 11:17:38 +0000

    add culture-specific formatting

--- src/Utils/CultureUtils.cs
+++ src/Utils/CultureUtils.cs
@@ -1,6 +1,7 @@
 ﻿// DragonFruit.Data Copyright DragonFruit Network
 // Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
 
+using System;
 using System.Globalization;
 
 namespace DragonFruit.Data.Utils
@@ -17,10 +18,12 @@ namespace DragonFruit.Data.Utils
 
         internal static string AsString(this object value, CultureInfo culture = null)
         {
+            culture ??= DefaultCulture;
             return value switch
             {
-                bool boolVar => boolVar.ToString().ToLower(culture ?? DefaultCulture),
                 null => null,
+                bool boolVar => boolVar.ToString().ToLower(),
+                IFormattable formattableVar => formattableVar.ToString(null, culture),
 
                 _ => value.ToString()
             };
