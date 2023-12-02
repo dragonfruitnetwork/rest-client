b4a683755c2f00310d26cb308020a156f22ad140
Albie Spriddell <albion@dragonfruit.network>
2023-12-02 20:43:35 +0000

    add copies of enums

---
+++ DragonFruit.Data.Roslyn/DragonFruit.Data.Roslyn/Generators/CollectionOptions.cs
@@ -0,0 +1,31 @@
+// DragonFruit.Data Copyright DragonFruit Network
+// Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
+
+namespace DragonFruit.Data.Roslyn.Generators
+{
+    /// <summary>
+    /// Copy of <see cref="EnumOptions"/> for use in the generator
+    /// </summary>
+    public enum CollectionOptions
+    {
+        /// <summary>
+        /// The query name is repeated and a new element created for each (a=1&a=2&a=3)
+        /// </summary>
+        Recursive,
+
+        /// <summary>
+        /// The query name has indexer symbols appended with no order (a[]=1&a[]=2&a[]=3)
+        /// </summary>
+        Unordered,
+
+        /// <summary>
+        /// The query name has indexer symbols appended explicit order inserted (a[0]=1&a[1]=2&a[2]=3)
+        /// </summary>
+        Indexed,
+
+        /// <summary>
+        /// The query is concatenated with a string and merged with one key (a=1,2,3)
+        /// </summary>
+        Concatenated
+    }
+}
---
+++ DragonFruit.Data.Roslyn/DragonFruit.Data.Roslyn/Generators/EnumOptions.cs
@@ -0,0 +1,16 @@
+// DragonFruit.Data Copyright DragonFruit Network
+// Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
+
+namespace DragonFruit.Data.Roslyn.Generators
+{
+    /// <summary>
+    /// Copy of <see cref="EnumOptions"/> for use in the generator
+    /// </summary>
+    public enum EnumOptions
+    {
+        None = 0,
+        Numeric = 1,
+        StringLower = 2,
+        StringUpper = 3,
+    }
+}
