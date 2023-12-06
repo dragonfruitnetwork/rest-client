580ef6af28c1a822b66eb3212b1d83b3205b3a17
Albie Spriddell <albion@dragonfruit.network>
2023-12-06 17:41:26 +0000

    add body type enum and attribute

---
+++ src/Requests/FormBodyType.cs
@@ -0,0 +1,11 @@
+// DragonFruit.Data Copyright DragonFruit Network
+// Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
+
+namespace DragonFruit.Data.Requests
+{
+    public enum FormBodyType
+    {
+        UriEncoded,
+        MultipartForm
+    }
+}
