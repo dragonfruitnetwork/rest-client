7c219004c750b5e96167d3f0b7894d8d091a06fb
Albie Spriddell <albion@dragonfruit.network>
2023-12-12 13:20:28 +0000

    hide sourcegen interface from intellisense

--- DragonFruit.Data/Requests/IRequestBuilder.cs
+++ DragonFruit.Data/Requests/IRequestBuilder.cs
@@ -1,11 +1,13 @@
 // DragonFruit.Data Copyright DragonFruit Network
 // Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
 
+using System.ComponentModel;
 using System.Net.Http;
 using DragonFruit.Data.Serializers;
 
 namespace DragonFruit.Data.Requests
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public interface IRequestBuilder
     {
         HttpRequestMessage BuildRequest(SerializerResolver serializerResolver);
