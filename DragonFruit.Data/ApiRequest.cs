261defd884a139eb4973e77a598ad8c2ea736bc6
Albie Spriddell <albion@dragonfruit.network>
2023-12-12 10:23:29 +0000

    add xmldoc to apirequest

--- DragonFruit.Data/ApiRequest.cs
+++ DragonFruit.Data/ApiRequest.cs
@@ -2,13 +2,31 @@
 // Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
 
 using System.Net.Http;
+using System.Threading.Tasks;
+using DragonFruit.Data.Requests;
 
 namespace DragonFruit.Data
 {
+    /// <summary>
+    /// The base class for all requests.
+    /// Properties and methods decorated with <see cref="RequestParameterAttribute"/> are used to build the request.
+    /// </summary>
     public abstract class ApiRequest
     {
+        /// <summary>
+        /// The base url of the request
+        /// </summary>
         public abstract string RequestPath { get; }
 
+        /// <summary>
+        /// The <see cref="HttpMethod"/> to use when making the request
+        /// </summary>
         public virtual HttpMethod RequestMethod => HttpMethod.Get;
+
+        /// <summary>
+        /// Overridable method to be called when the request is about to be built.
+        /// This can be used to inject headers with data managed by the client, etc.
+        /// </summary>
+        protected virtual ValueTask RequestCreatingCallback() => default;
     }
 }
