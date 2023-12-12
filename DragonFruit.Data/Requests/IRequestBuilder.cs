1bdbd26ddad7863e4f472c2fd3fde871b697a88f
Albie Spriddell <albion@dragonfruit.network>
2023-12-12 10:29:59 +0000

    add resolver to request builder template

--- DragonFruit.Data/Requests/IRequestBuilder.cs
+++ DragonFruit.Data/Requests/IRequestBuilder.cs
@@ -2,11 +2,12 @@
 // Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
 
 using System.Net.Http;
+using DragonFruit.Data.Serializers;
 
 namespace DragonFruit.Data.Requests
 {
     public interface IRequestBuilder
     {
-        HttpRequestMessage BuildRequest();
+        HttpRequestMessage BuildRequest(SerializerResolver serializerResolver);
     }
 }
