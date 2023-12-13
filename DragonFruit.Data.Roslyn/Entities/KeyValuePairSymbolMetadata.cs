ed7fee7fb7a1433823d75ad08569694f88081100
Albie Spriddell <albion@dragonfruit.network>
2023-12-13 10:47:00 +0000

    add key-value pair writer

---
+++ DragonFruit.Data.Roslyn/Entities/KeyValuePairSymbolMetadata.cs
@@ -0,0 +1,18 @@
+﻿// DragonFruit.Data Copyright DragonFruit Network
+// Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
+
+using DragonFruit.Data.Roslyn.Enums;
+using Microsoft.CodeAnalysis;
+
+namespace DragonFruit.Data.Roslyn.Entities
+{
+    internal class KeyValuePairSymbolMetadata : PropertySymbolMetadata
+    {
+        public override RequestSymbolType Type => RequestSymbolType.KeyValuePair;
+
+        public KeyValuePairSymbolMetadata(ISymbol symbol, ITypeSymbol returnType, string parameterName)
+            : base(symbol, returnType, parameterName)
+        {
+        }
+    }
+}
