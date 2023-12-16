971709b41cc0310d6cda3a8d661450748c7eb3b4
Albie Spriddell <albion@dragonfruit.network>
2023-12-16 17:37:19 +0000

    move serializers around

--- DragonFruit.Data.Serializers.Html/HtmlSerializer.cs
+++ DragonFruit.Data.Serializers.Html/HtmlSerializer.cs
@@ -1,6 +1,9 @@
 // DragonFruit.Data Copyright DragonFruit Network
 // Licensed under the MIT License. Please refer to the LICENSE file at the root of this project for details
 
+using System;
+using System.IO;
+using System.Net.Http;
 using HtmlAgilityPack;
 
 namespace DragonFruit.Data.Serializers.Html
